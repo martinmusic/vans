1c6cc4c9179f8948e723bd5b3b9c360472bc42e5
martinmusic <140605951+martinmusic@users.noreply.github.com>
2025-01-23 12:18:00 +0700

    Update install.sh keep resize_4 only

--- install.sh
+++ install.sh
@@ -28,62 +28,6 @@ URL=${1:-https://a.301edge.com}
 
 # 创建并配置启动脚本
 cat << EOF > /etc/supervisor/conf.d/http.conf
-# [program:httpstatic]
-# command=${SCRIPT_DIR}/httpstatic ${URL}
-# user=root
-# stderr_logfile=/var/log/httpstatic.err.log
-# stdout_logfile=/var/log/httpstatic.out.log
-# stdout_logfile_maxbytes=1MB
-# stderr_logfile_maxbytes=1MB
-# autorestart=true
-# startretries=3
-# startsecs=0
-# exitcodes=0,2
-# stopsignal=TERM
-# stopwaitsecs=10
-
-[program:tcpresize_1]
-command=${SCRIPT_DIR}/tcpresize -v --queue=1
-user=root
-stderr_logfile=/var/log/tcpresize_1.err.log
-stdout_logfile=/var/log/tcpresize_1.out.log
-stdout_logfile_maxbytes=1MB
-stderr_logfile_maxbytes=1MB
-autorestart=true
-startretries=3
-startsecs=0
-exitcodes=0,2
-stopsignal=TERM
-stopwaitsecs=10
-
-[program:tcpresize_2]
-command=${SCRIPT_DIR}/tcpresize -v --queue=2
-user=root
-stderr_logfile=/var/log/tcpresize_2.err.log
-stdout_logfile=/var/log/tcpresize_2.out.log
-stdout_logfile_maxbytes=1MB
-stderr_logfile_maxbytes=1MB
-autorestart=true
-startretries=3
-startsecs=0
-exitcodes=0,2
-stopsignal=TERM
-stopwaitsecs=10
-
-[program:tcpresize_3]
-command=${SCRIPT_DIR}/tcpresize -v --queue=3
-user=root
-stderr_logfile=/var/log/tcpresize_3.err.log
-stdout_logfile=/var/log/tcpresize_3.out.log
-stdout_logfile_maxbytes=1MB
-stderr_logfile_maxbytes=1MB
-autorestart=true
-startretries=3
-startsecs=0
-exitcodes=0,2
-stopsignal=TERM
-stopwaitsecs=10
-
 [program:tcpresize_4]
 command=${SCRIPT_DIR}/tcpresize -v --queue=4
 user=root
